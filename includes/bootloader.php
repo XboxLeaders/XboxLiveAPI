f36395510d51c2caa7ff3fecf7c43f860382134c
Jason Clemons <jason@xboxleaders.com>
2014-01-17 15:24:36 -0500

    ! Update file info

--- includes/bootloader.php
+++ includes/bootloader.php
@@ -1,15 +1,17 @@
 <?php
-/*******************************************************************************
- * XboxLeaders Xbox LIVE REST API                                              *
- * =========================================================================== *
- * @file        bootloader.php                                                 *
- * @package     XboxLiveAPI                                                    *
- * @version     2.0                                                            *
- * @copyright   (c) 2013 - Jason Clemons <me@jasonclemons.me>                  *
- * @contributor Alan Wynn <http://github.com/djekl>                            *
- * @contributor Luke Zbihlyj <http://github.com/lukezbihlyj>                   *
- * @license     http://opensource.org/licenses/mit-license.php The MIT License *
- *******************************************************************************/
+/**
+* This is the main configuration file for the API.
+*
+* LICENSE: MIT (http://opensource.org/licenses/mit-license.html)
+*
+* @category     XboxLeaders
+* @package      XboxLiveAPI
+* @copyright    Copyright (c) 2012 - 2014 XboxLeaders
+* @license      http://xboxleaders.github.io/license/ MIT License
+* @version      2.0
+* @link         http://github.com/XboxLeaders/XboxLiveAPI
+* @since        File available since Release 1.0
+*/
 
 include('classes/cache.class.php');
 include('classes/base.class.php');
